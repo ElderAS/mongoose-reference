d3c9c16552fed9daaa6f27b53f91594f2e35ac70
Carsten Jacobsen <car.jacobsen@gmail.com>
2019-10-23 15:54:53 +0200

    fix(index): adding required prop

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const Schema = require('mongoose').Schema
 
 module.exports = function(options = {}) {
-  let { models, multiple = false, key } = options
+  let { models, multiple = false, key, required = true } = options
   if (!key) key = multiple ? 'references' : 'reference'
 
   let reference_content = {
     on: {
       type: Schema.Types.ObjectId,
-      required: true,
+      required,
       refPath: key + '.onModel',
     },
     onModel: {
       type: String,
-      required: true,
-      enum: models ? models : undefined,
+      required,
+      enum: models,
     },
   }
 
